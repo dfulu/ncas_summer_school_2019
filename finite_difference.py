d197e81faefed97c97ddecfb451b7aa510c7b301
dfulu <djamesfulton@yahoo.co.uk>
2019-09-10 15:14:34 +0100

    broken changes

--- finite_difference.py
+++ finite_difference.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import animatplot as amp
 
 # constants
 r = 6371e3 # radius of earth in meters
@@ -43,7 +44,7 @@ def run(nx, nt, nsave, g=9.81, T=1e4):
     
     # set initial conditions
     u0 = np.cos(np.linspace(0,1,nx)*2*np.pi)
-    h0 = np.cos(np.linspace(0,1,nx)*2*np.pi)
+    h0 = np.cos(np.linspace(0,1,nx)*2*np.pi)+2
     
     # set store for those values to save
     u_all = np.zeros((nt//nsave, nx))
@@ -83,39 +84,56 @@ def run(nx, nt, nsave, g=9.81, T=1e4):
             h_all[i//nsave] = h
             
     return u_all, h_all
-        
+     
 
 if __name__=="__main__":
-    nx = 1000
+    # set parameters
+    T=1e8 # make this 2 powers above nt
     nt = int(1e6)
-    nsave=1000
-    u_all, h_all = run(nx, nt, nsave, g=9.81, T=1e6)
+    nframes = 1000
+    dtbydx=0.1
+    make_plot=False
     
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import animatplot as amp
+    # derived quantities
+    nx = int(dtbydx*diam*nt/T)
+    print(nx)
+    nsave = nt//nframes
     
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    # run simulation
+    u_all, h_all = run(nx, nt, nsave, g=9.81, T=T)
+    
+    if make_plot:
+        # set up fig and axes
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        for ax in [ax1, ax2]:
+            ax.set_xlabel('x')
+        ax1.set_ylabel('u')
+        ax2.set_ylabel('h')
 
-    for ax in [ax1, ax2]:
-        #ax.set_aspect('equal')
-        ax.set_xlabel('x')
+        minu, maxu = np.nanmin(u_all), np.nanmax(u_all)
+        minh, maxh = np.nanmin(h_all), np.nanmax(h_all)
+        minu = minu if minu>-np.inf else -1
+        maxu = maxu if maxu<np.inf else 1
+        minh = minh if minh>-np.inf else 0
+        maxh = maxh if maxh<np.inf else 3
+        ax1.set_ylim(minu, maxu)
+        ax2.set_ylim(minh, maxh)
 
-    ax1.set_ylabel('u')
-    ax2.set_ylabel('h')
-    
-    t = np.arange(0, nt, nsave)
-    x = np.linspace(0, diam, nx)
-    X = np.repeat(x[:, np.newaxis], nt//nsave, 1).T
+        # time and space info for axis
+        t = np.arange(0, nt, nsave)
+        x = np.linspace(0, diam, nx)
+        X = np.repeat(x[:, np.newaxis], nt//nsave, 1).T
 
-    u_block = amp.blocks.Line(X, u_all, ax=ax1)
-    h_block = amp.blocks.Line(X, h_all, ax=ax2)
-    
-    timeline = amp.Timeline(t, units='s', fps=20)
-    anim = amp.Animation([u_block, h_block], timeline = timeline)
-    plt.tight_layout()
-    anim.controls()
-    anim.save_gif('line1') # save animation for docs
-    plt.show()
-    print('DONE')
-    
+        # create variable blocks and timeline
+        u_block = amp.blocks.Line(X, u_all, ax=ax1)
+        h_block = amp.blocks.Line(X, h_all, ax=ax2)
+        timeline = amp.Timeline(t, units='s', fps=20)
+
+        # Make gif
+        anim = amp.Animation([u_block, h_block], timeline = timeline)
+        plt.tight_layout()
+        anim.controls()
+        anim.save_gif('line1') # save animation for docs
+        plt.show()
+        print('DONE')
+
