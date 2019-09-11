fix frame saving, fix rmse calculation, and add run save functionality

comparison.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.interpolate import interp1d
+from datetime import datetime
+import pickle
 from finite_difference import *
 
 T=5e6 # make this 2 powers above nt
@@ -6,18 +9,35 @@ Nt = int(1e4) # was 1e5
 Nx=100
 nframes = 200
 
-def calc_rmse(x,y):
+def calc_rmse(y1,y2):
     '''
     presume y is longer
     '''
-    minlen = min(x.shape[1], y.shape[1])
-    maxlen = max(x.shape[1], y.shape[1])
-    skip = maxlen//minlen
-    return ((y[:,::skip][:,:len(x)] -
-             x[:,:len(y[:,::skip])])**2).mean()**0.5
+    x1 = np.linspace(0,1,y1.shape[1], endpoint=False)
+    x2 = np.linspace(0,1,y2.shape[1]+1)
+    y2_ = np.append(y2, y2[:,:1], axis=1)
+    f = interp1d(x2,y2_, kind='quadratic', axis=1)
+    y2_int = f(x1)
+    return ((y1 - y2_int)**2).mean()**0.5
 
 def calc_all_rsmes(x, base, factors):
     return [calc_rmse(x[i], base) for i in range(len(factors)-1)]
+
+def save_lists(filename,
+    u_ctcs_list, h_ctcs_list, 
+    u_ftbtcs_list, h_ftbtcs_list
+              ):
+    # save the data 
+    all_lists={
+        'u_ctcs_list':u_ctcs_list, 
+        'h_ctcs_list':h_ctcs_list, 
+        'u_ftbtcs_list':u_ftbtcs_list,
+        'habs_ftbtcs_list':h_ftbtcs_list
+    }
+
+    with open(filename, 'wb') as handle:
+        pickle.dump(all_lists, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    return
 
 # data stores
 u_ctcs_list = []
@@ -25,7 +45,8 @@ h_ctcs_list = []
 u_ftbtcs_list = []
 h_ftbtcs_list = []
     
-factors = [f for f in 10.**np.arange(-1,3, 0.5)]
+
+factors = [f for f in 10.**np.arange(-1,2.75, 0.25)]
 # [f for f in 10.**np.arange(-1,2)] 
     
 for factor in factors:
@@ -36,23 +57,32 @@ for factor in factors:
     
     # derived quantities
     dtbydx = (T/nt)/(diam/nx)
-    nsave = nt//nframes
     
     print('-'*10)
+    print(datetime.now())
     print("dt/dx:", dtbydx)
     courant = dtbydx*np.sqrt(2000*9.81)
     print("c:", courant)
     print('nx:', nx)
-    print('nx:', nt)
+    print('nt:', nt)
     
     # run simulation
-    u_ctcs, h_ctcs = run_ctcs(nx, nt, nsave, g=9.81, T=T)
-    u_ftbtcs, h_ftbtcs = run_ftbtcs(nx, nt, nsave, g=9.81, T=T)
+    u_ctcs, h_ctcs = run_ctcs(nx, nt, nframes, g=9.81, T=T)
+    u_ftbtcs, h_ftbtcs = run_ftbtcs(nx, nt, nframes, g=9.81, T=T)
     
     u_ctcs_list.append(u_ctcs)
     h_ctcs_list.append(h_ctcs)
     u_ftbtcs_list.append(u_ftbtcs)
     h_ftbtcs_list.append(h_ftbtcs)
+    
+    # save
+    print('saving : results_data_{:.2f}.pkl'.format(factor))
+    save_lists(
+        'results_data_{:.2f}.pkl'.format(factor),
+        u_ctcs_list, h_ctcs_list, 
+        u_ftbtcs_list, h_ftbtcs_list
+    )
+    
 
 # calculate RSME for all the runs from the best run
 u_ctcs_rmse = calc_all_rsmes(u_ctcs_list, u_ctcs_list[-1], factors)

finite_difference.py
@@ -61,7 +61,7 @@ def evolve_h_btcs(h, u_plus, dtbydx):
     return dtbydx*(term1+term2) + h
 
 
-def run_ctcs(nx, nt, nsave, g=9.81, T=1e4):
+def run_ctcs(nx, nt, nframes, g=9.81, T=1e4):
     """
     Runs CTCS on an A-grid
     
@@ -76,8 +76,10 @@ def run_ctcs(nx, nt, nsave, g=9.81, T=1e4):
     h0 = np.cos(np.linspace(0,1,nx)*2*np.pi)+2000
     
     # set store for those values to save
-    u_all = np.zeros((nt//nsave, nx))
-    h_all = np.zeros((nt//nsave, nx))
+    u_all = np.zeros((nframes, nx))
+    h_all = np.zeros((nframes, nx))
+    savewhen = np.linspace(0,1,nframes)
+    j=1
     u_all[0] = u0
     h_all[0] = h0
     
@@ -107,14 +109,15 @@ def run_ctcs(nx, nt, nsave, g=9.81, T=1e4):
         h = h_plus
         
         # save vaklues if on save run
-        if (i%nsave)==0:
-            u_all[i//nsave] = u
-            h_all[i//nsave] = h
+        if (i/nt)>=savewhen[j]:
+            u_all[j] = u
+            h_all[j] = h
+            j+=1
             
     return u_all, h_all
 
 
-def run_ftbtcs(nx, nt, nsave, g=9.81, T=1e4):
+def run_ftbtcs(nx, nt, nframes, g=9.81, T=1e4):
     """
     Runs semi-implicit integration on the A-grid
     Ags:
@@ -128,8 +131,10 @@ def run_ftbtcs(nx, nt, nsave, g=9.81, T=1e4):
     h0 = np.cos(np.linspace(0,1,nx)*2*np.pi)+2000
     
     # set store for those values to save
-    u_all = np.zeros((nt//nsave, nx))
-    h_all = np.zeros((nt//nsave, nx))
+    u_all = np.zeros((nframes, nx))
+    h_all = np.zeros((nframes, nx))
+    savewhen = np.linspace(0,1,nframes)
+    j=1
     u_all[0] = u0
     h_all[0] = h0
     
@@ -158,9 +163,10 @@ def run_ftbtcs(nx, nt, nsave, g=9.81, T=1e4):
         h = h_plus
         
         # save vaklues if on save run
-        if (i%nsave)==0:
-            u_all[i//nsave] = u
-            h_all[i//nsave] = h
+        if (i/nt)>=savewhen[j]:
+            u_all[j] = u
+            h_all[j] = h
+            j+=1
             
     return u_all, h_all
      
@@ -168,10 +174,10 @@ def run_ftbtcs(nx, nt, nsave, g=9.81, T=1e4):
 if __name__=="__main__":
     # set parameters
     T=5e6 # make this 2 powers above nt
-    nt = int(1e5)
+    nt = int(1e4)
     nframes = 200
     dtbydx=0.1
-    make_plot=True
+    make_plot=False
     nx=100
     gifname = "ctcs-ftbtcs1"
     
@@ -227,4 +233,13 @@ if __name__=="__main__":
         plt.show()
         
     print('DONE')
+    
+    
+"""
+funtion to test lengths of saved frames
+def f(nt, nsave):
+    x0 = np.zeros((nt//nsave+1*((nt%nsave)!=0), nx)).shape[0]
+    x1 = [i//nsave for i in np.arange(nt) if i%nsave==0][-1]
+    print(x0,x1)
+"""
 